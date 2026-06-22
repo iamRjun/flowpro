57bc8a7de44485cd60bd8cc9c3f1859a51aabf69
Arjun <iamarjun404@gmail.com>
2026-06-22 18:59:28 +0545

    Build basic form

---
+++ src/pages/Login.tsx
@@ -0,0 +1,18 @@
+function Login() {
+  return (
+    <div>
+      <div>
+        <h1>Welcome Back</h1>
+        <p>Sign in to your account</p>
+        <br />
+        <input type="email" id="email" placeholder="Email Address" />
+        <br />
+        <input type="password" id="password" placeholder="Password" />
+        <br />
+        <button>Login</button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
