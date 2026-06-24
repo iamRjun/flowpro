1308027f47455a5c5c0c4649090772ee4ceef9f9
Arjun <iamarjun404@gmail.com>
2026-06-24 18:46:36 +0545

    added all the essentials elements in the form not styled yet

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 function Login() {
   return (
-    <div>
-      <div>
+    <div className="min-h-screen flex items-center justify-center bg-[#9FA1FF]">
+      <div className="bg-[#B5BAFF] p-8 rounded-2xl text-white shadow-2xl  border-2 border-white">
         <h1>Welcome Back</h1>
         <p>Sign in to your account</p>
         <br />
@@ -9,7 +9,31 @@ function Login() {
         <br />
         <input type="password" id="password" placeholder="Password" />
         <br />
-        <button>Login</button>
+        <label>
+          <input type="checkbox" id="remember" />
+          Remember me
+        </label>
+        <a className="text-blue-500" href="https://www.w3schools.com/">
+          Forgot password?
+        </a>
+        <button className="bg-blue-500 text-white p-4 rounded">Sign In</button>
+        <div className="relative flex items-center my-6">
+          <div className="flex-grow border-t-2 border-white/30"></div>
+          <span className="flex-shrink mx-4 text-white/80 text-sm font-medium">
+            or continue with
+          </span>
+          <div className="flex-grow border-t-2 border-white/30"></div>
+        </div>
+        <div className="flex items-center justify-center gap-20">
+          <button>Google</button>
+          <button>GitHub</button>
+        </div>
+        <div className="flex items-center gap-2">
+          <p className="text-white/80">Don't have an account?</p>
+          <a className="text-blue-500" href="https://www.w3schools.com/">
+            Sign up
+          </a>
+        </div>
       </div>
     </div>
   );
